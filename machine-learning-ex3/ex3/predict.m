71fdfadebd56b510e959a9447f4605c91b7b195c
Gogs <gogs@fake.local>
2018-07-15 02:44:51 -0400

    fixed neural network function adding missing sigmoid call increased accuracy from 69 to 97

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -21,11 +21,11 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-X = [ones(m, 1), X];
-Xtheta1 = [ones(m, 1) X*Theta1'];
-[X, ix] = max(Xtheta1*Theta2', [], 2);
+a1 = [ones(m, 1), X];
+a2 = [ones(m, 1) sigmoid(a1*Theta1')];
+[a3, maxIndex] = max(sigmoid(a2*Theta2'), [], 2);
 
-p = ix;
+p = maxIndex;
 
 
 
